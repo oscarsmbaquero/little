72152b258482155f3cd542f46b9d1ce3b99a384f
oscarsmbaquero <oscarsmbaquero@gmail.com>
2025-07-01 22:13:21 +0200

    ajuste reloj

--- src/pages/fichaje/fichaje.component.ts
+++ src/pages/fichaje/fichaje.component.ts
@@ -6,7 +6,7 @@ import { ButtonModule } from 'primeng/button';
   standalone: true,
   imports: [ButtonModule],
   templateUrl: './fichaje.component.html',
-  styleUrl: './fichaje.component.css'
+  styleUrl: './fichaje.component.css',
 })
 export class FichajeComponent implements OnInit, OnDestroy {
   horaEntrada: string | null = null;
@@ -17,16 +17,15 @@ export class FichajeComponent implements OnInit, OnDestroy {
   private intervalId: any;
   user: any;
 
-    ngOnInit() {
+  ngOnInit() {
     this.actualizarReloj();
     this.intervalId = setInterval(() => this.actualizarReloj(), 1000);
     this.user = JSON.parse(localStorage.getItem('user') || '{}');
   }
 
-   ngOnDestroy() {
+  ngOnDestroy() {
     clearInterval(this.intervalId);
   }
-  
 
   private obtenerHoraActual(): string {
     const ahora = new Date();
@@ -35,13 +34,11 @@ export class FichajeComponent implements OnInit, OnDestroy {
 
   ficharEntrada(): void {
     this.horaEntrada = this.obtenerHoraActual();
-    // Aquí puedes también enviar al backend si quieres
     console.log('Entrada fichada a las:', this.horaEntrada);
   }
 
   ficharSalida(): void {
     this.horaSalida = this.obtenerHoraActual();
-    // Aquí puedes también enviar al backend si quieres
     console.log('Salida fichada a las:', this.horaSalida);
   }
 
@@ -55,5 +52,4 @@ export class FichajeComponent implements OnInit, OnDestroy {
     this.minuteTransform = `rotate(${min * 6 + seg * 0.1}deg)`;
     this.hourTransform = `rotate(${(hora % 12) * 30 + min * 0.5}deg)`;
   }
-
 }
