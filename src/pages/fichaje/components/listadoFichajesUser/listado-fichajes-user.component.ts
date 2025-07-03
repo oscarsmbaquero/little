0e8276ad400142e20735e22567d20d2507374c5c
oscarsmbaquero <oscarsmbaquero@gmail.com>
2025-07-03 22:07:59 +0200

    geolocation service

--- src/pages/fichaje/components/listadoFichajesUser/listado-fichajes-user.component.ts
+++ src/pages/fichaje/components/listadoFichajesUser/listado-fichajes-user.component.ts
@@ -1,40 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { FichajesService } from '../../../../core/services/fichajes/fichajes.service';
+import { GeolocationService } from '../../../../core/services/GeolocationService/geolocation.service';
 
 @Component({
   selector: 'app-listado-fichajes-user',
   standalone: true,
   imports: [],
   templateUrl: './listado-fichajes-user.component.html',
-  styleUrl: './listado-fichajes-user.component.css'
+  styleUrl: './listado-fichajes-user.component.css',
 })
 export class ListadoFichajesUserComponent implements OnInit {
-
-  user = ''
-  tienda= ''
+  user = '';
+  tienda = '';
   idUsuario!: number;
   fichajes: any[] = [];
+  direccion: string | null = null;
 
-  constructor(private fichajesService: FichajesService) {}
-
-
+  constructor(
+    private fichajesService: FichajesService,
+    private geolocationService: GeolocationService
+  ) {}
 
   ngOnInit() {
     const usuario = JSON.parse(localStorage.getItem('user') || '{}');
     this.user = usuario.data.user;
     this.tienda = usuario.data.tienda;
-    this.idUsuario = usuario.data.idUsuario; 
-    
-    this.fichajesService.getFichajesByUser(this.idUsuario)
-      .subscribe((response: any) => {
-        console.log('Fichajes obtenidos:', response);
+    this.idUsuario = usuario.data.idUsuario;
+
+    this.fichajesService.getFichajesByUser(this.idUsuario).subscribe(
+      (response: any[]) => {
         this.fichajes = response;
-        console.log(this.fichajes);
-        
-      }, (error) => {
+
+        this.fichajes.forEach((fichaje, index) => {
+          const { lat, lng } = fichaje.entrada;
+
+          if (lat && lng) {
+            this.geolocationService
+              .getLocalidadFromCoordinates(lat, lng)
+              .subscribe((localidad) => {
+                this.fichajes[index].localidadEntrada = localidad;
+              });
+          } else {
+            this.fichajes[index].localidadEntrada = 'No disponible';
+          }
+        });
+      },
+      (error) => {
         console.error('Error al obtener los fichajes:', error);
-      });
-
+      }
+    );
   }
-
 }
