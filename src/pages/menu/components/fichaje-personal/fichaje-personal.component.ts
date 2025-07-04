13a8125d814b5c4741f64266341c445c17e2e803
oscarsmbaquero <oscarsmbaquero@gmail.com>
2025-07-04 21:47:46 +0200

    menu ok

---
+++ src/pages/menu/components/fichaje-personal/fichaje-personal.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+import { FichajesService } from '../../../../core/services/fichajes/fichajes.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-fichaje-personal',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './fichaje-personal.component.html',
+  styleUrl: './fichaje-personal.component.css'
+})
+export class FichajePersonalComponent {
+
+  fichajesService = inject(FichajesService);
+
+  opcionSeleccionada: string = '';
+  opciones = [
+    { valor: 'opcion1', label: 'Opción 1' },
+    { valor: 'opcion2', label: 'Opción 2' },
+    { valor: 'opcion3', label: 'Opción 3' }
+  ];
+
+  constructor() { }
+
+}
