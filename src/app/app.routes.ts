90bba8e403d32c6d46e76a19794cb975f842fa9c
oscarsmbaquero <oscarsmbaquero@gmail.com>
2025-07-04 21:49:45 +0200

    menu ok II

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FichajeComponent } from '../pages/fichaje/fichaje.component';
 import { LoginComponent } from '../core/components/login/login.component';
+import { MenuComponent } from '../pages/menu/menu.component';
+import { FichajePersonalComponent } from '../pages/menu/components/fichaje-personal/fichaje-personal.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +14,16 @@ export const routes: Routes = [
     path: "fichaje",//raiz de la app
     pathMatch: 'full',//coincida nombre exacto
     component: FichajeComponent
+  },
+  {
+    path: "menu",//raiz de la app
+    pathMatch: 'full',//coincida nombre exacto
+    component: MenuComponent
+  },
+   {
+    path: "fichaje-personal",//raiz de la app
+    pathMatch: 'full',//coincida nombre exacto
+    component: FichajePersonalComponent
   },
   {
     path: "",//raiz de la app
